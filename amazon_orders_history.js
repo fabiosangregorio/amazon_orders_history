787398ac11453823971970a3295a27e50c6a57f9
Fabio <fabio.sangregorio@gmail.com>
2019-11-18 22:17:26 +0100

    Add returned items sum, add readme

--- amazon_orders_history.js
+++ amazon_orders_history.js
@@ -1,12 +1,111 @@
-(function () {
+// ==UserScript==
+// @name         Amazon Order History
+// @namespace    https://fabio.sangregorio.dev
+// @version      0.1
+// @description  Surprise yourself by discovering how much you spent on all of your Amazon orders.
+// @author       Fabio Sangregorio
+// @match        https://www.amazon.it/gp/your-account/order-history*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  const storage = localStorage;
+  const settings = {
+    baseUrl: window.location.protocol + "//" + window.location.host + "/gp/your-account/order-history",
+    orderSelector: '.order',
+    amountSelector: '.order-info .a-span2 .value',
+    nextPageSelector: '.a-selected + .a-normal'
+  };
+  
+  let state = getStorageItem('amz_orders_history') || {
+    process: {},
+    years: {}
+  };
+  const currentYear = Number(getParameterByName('orderFilter').replace('year-', ''));
+  const pageHasOrders = !!document.querySelectorAll(settings.orderSelector).length;
+  
+  run();
+  
+  function run() {
+    // Start
+    if (pageHasOrders && state.process.status !== 'running') {
+      state = {
+        process: {
+          status: 'running',
+          startYear: currentYear,
+          endYear: null,
+          currency: document.querySelector(settings.amountSelector).innerText.split(' ')[0]
+        },
+        years: {}
+      };
+    }
+  
+    // Stop
+    if (!pageHasOrders && state.process.status === 'running') {
+      state.process.status = 'complete';
+      saveState(state);
+    }
+  
+    if (!pageHasOrders && state.process.status === 'complete')
+      showStats();
+  
+    checkPage();
+  }
+  
+  function checkPage() {
+    if (state.process.status !== 'running') return;
+  
+    // Add page orders to storage
+    const orders = state.years[currentYear] || [];
+    for (const node of document.querySelectorAll(settings.orderSelector)) {
+      orders.push({
+        amount: currToNumber(node.querySelector(settings.amountSelector).innerText),
+        returned: !!node.querySelector('[href*="returns"]')
+      });
+    }
+    state.years[currentYear] = orders;
+    saveState(state);
+  
+    // Go to next page
+    const nextPage = document.querySelector(settings.nextPageSelector);
+    if (!!nextPage) location.href = nextPage.children[0].href;
+    else location.href = `${settings.baseUrl}?orderFilter=year-${currentYear - 1}`;
+  }
+  
+  function showStats() {
+    state.process.status = 'complete';
+    state.process.endYear = currentYear;
+    saveState(state);
+  
+    let ordersSum = 0;
+    let returnedSum = 0;
+    for (let year of Object.values(state.years)) {
+      ordersSum += year.filter(o => !o.returned).reduce((t, o) => t + o.amount, 0);
+      returnedSum += year.filter(o => o.returned).reduce((t, o) => t + o.amount, 0);
+    }
+  
+    const currency = state.process.currency;
+    console.log(`
+      You spent ${currency} ${ordersSum.toFixed(2)} in
+      ${state.process.startYear - state.process.endYear} years, and returned
+      ${currency} ${returnedSum.toFixed(2)} worth of items.
+    `.replace(/\s+/gm, ' '));
+  }
+
+  
+  // UTILS FUNCTIONS
   function setStorageItem(key, value) {
     storage.setItem(key, JSON.stringify(value));
   }
-
+  
   function getStorageItem(key) {
     return JSON.parse(storage.getItem(key));
   }
-
+  
+  function saveState(state) {
+    setStorageItem('amz_orders_history', state);
+  }
+  
   function currToNumber(curr) {
     return Number(curr
       .replace('EUR ', '')
@@ -14,7 +113,7 @@
       .replace('.', '')
       .replace(',', '.'));
   }
-
+  
   /**
    * Gets the value of a query parameter from the url
    * @param {String} name name of the parameter to get
@@ -29,78 +128,4 @@
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
   }
-
-  const storage = localStorage;
-  const settings = {
-    baseUrl: window.location.protocol + "//" + window.location.host + "/gp/your-account/order-history",
-    orderSelector: '.order',
-    amountSelector: '.order-info .a-span2 .value',
-    nextPageSelector: '.a-selected + .a-normal'
-  };
-
-  let process = getStorageItem('amz_process');
-  const currentYear = Number(getParameterByName('orderFilter').replace('year-', ''));
-  const pageHasOrders = !!document.querySelectorAll(settings.orderSelector).length;
-
-  // Start
-  if (pageHasOrders && (!process || process.status !== 'running')) {
-    // Clear storage
-    for (let key of Object.keys(storage)) {
-      if (!key.includes('amz_orders_')) continue;
-      storage.removeItem(key);
-    }
-    process = {
-      'status': 'running',
-      'start_year': currentYear,
-      'end_year': null
-    };
-    setStorageItem('amz_process', process);
-  }
-
-  // Stop
-
-  if (!pageHasOrders && process && process.status === 'running') {
-    process.status = 'complete';
-    setStorageItem('amz_process', process);
-  }
-
-  if (!pageHasOrders && process && process.status === 'complete') {
-    process.status = 'complete';
-    process.end_year = currentYear;
-    setStorageItem('amz_process', process);
-
-    const orders = Object.keys(storage)
-      .filter(k => k.includes('amz_orders_'))
-      .map(k => {
-        return {
-          year: Number(k.replace('amz_orders_', '')),
-          orders: getStorageItem(k)
-        };
-      });
-    
-    const totalSum = orders.reduce((total, year) => {
-      const yearSum = year.orders
-        .filter(o => !o.returned)
-        .reduce((yearSum, order) => yearSum + Number(order.amount), 0);
-      return total + yearSum;
-    }, 0);
-    console.log(totalSum);
-  }
-
-
-  if (process.status !== 'running') return;
-  // Add page orders to storage
-  const storageOrders = getStorageItem(`amz_orders_${currentYear}`) || [];
-  for (const node of document.querySelectorAll(settings.orderSelector)) {
-    storageOrders.push({
-      amount: currToNumber(node.querySelector(settings.amountSelector).innerText),
-      returned: !!node.querySelector('[href*="returns"]')
-    });
-  }
-  setStorageItem(`amz_orders_${currentYear}`, storageOrders);
-
-  // Go to next page
-  const nextPage = document.querySelector(settings.nextPageSelector);
-  if (!!nextPage) location.href = nextPage.children[0].href;
-  else location.href = `${settings.baseUrl}?orderFilter=year-${currentYear - 1}`;
 })();
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Amazon Orders History
+Crawls your Amazon orders history webpages in order to compute the total amount
+of your orders. The sum is shown in the Chrome Developers Tools console.
+
+## Prerequisites
+You need [Tampermonkey](https://chrome.google.comhttps://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en) to run this script.
+
+## Usage
+1. Create a new Tampermonkey script, paste the content of `amazon_orders_history.js` in the editor, save.
+1. Go to your Amazon orders page. 
+1. From the "last 6 months" orders selection, select this year.
+1. The script will start crawling all the orders web pages, this will take a moment.
+1. Open the Chrome Developer Tools. You will see the total amount in the "Console" tab.
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
